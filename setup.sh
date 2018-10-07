cdffbfe4629559979ea1158c7ad103466a5caed7
Ari Lotter <arilotter@gmail.com>
2018-10-07 05:16:59 -0400

    fix symlinking

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-ln -s ./nixpkgs ~/.config/nixpkgs
-ln -s ./wal ~/.config/wal
-ln -s ./i3/blocks blocks
-ln -s ./i3/bar.toml bar.toml
+if [[ ! -d $(realpath ~/.config/nixpkgs) ]]; then ln -s $(realpath ./nixpkgs) $(realpath ~/.config/nixpkgs); fi
+if [[ ! -d $(realpath ~/.config/wal) ]]; then ln -s $(realpath ./wal) $(realpath ~/.config/wal); fi
+if [[ ! -d $(realpath ~/.config/i3) ]]; then mkdir ~/.config/i3; fi
+if [[ ! -d $(realpath ~/.config/i3/blocks) ]]; then ln -s  $(realpath ./i3/blocks) $(realpath ~/.config/i3/blocks); fi
+if [[ ! -f $(realpath ~/.config/i3/bar.toml) ]]; then ln -s  $(realpath ./i3/bar.toml) $(realpath ~/.config/i3/bar.toml); fi
+if [[ ! -f $(realpath ~/bin/change-theme) ]]; then ln -s  $(realpath ./bin/change-theme) $(realpath ~/bin/change-theme); fi
