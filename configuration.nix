9d52c852dbf5efbd7f45cd4b67d7efe24fc8c87b
Ari Lotter <arilotter@gmail.com>
2019-04-11 10:35:45 -0400

    install tewi, remove thinkfan

--- configuration.nix
+++ configuration.nix
@@ -78,6 +78,7 @@
     noto-fonts-emoji
     liberation_ttf
     fira-code
+    tewi-font
     (nerdfonts.override {
       withFont = "--complete FiraCode";
     })
@@ -139,6 +140,8 @@
 
   virtualisation.lxd.enable = true;
 
+  # powerManagement.powerDownCommands = ''echo "`id` $SHELL AAAAAA I WENT TO SLEEP" > /dev/kmsg'';
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users = {
     extraUsers.ari = {
@@ -159,27 +162,5 @@
     CPU_SCALING_GOVERNOR_ON_BAT=powersave
     ENERGY_PERF_POLICY_ON_BAT=powersave
   '';
-  services.thinkfan = {
-    enable = true;
-    sensors = ''
-      hwmon /sys/devices/platform/coretemp.0/hwmon/hwmon3/temp3_input
-      hwmon /sys/devices/platform/coretemp.0/hwmon/hwmon3/temp4_input
-      hwmon /sys/devices/platform/coretemp.0/hwmon/hwmon3/temp1_input
-      hwmon /sys/devices/platform/coretemp.0/hwmon/hwmon3/temp5_input
-      hwmon /sys/devices/platform/coretemp.0/hwmon/hwmon3/temp2_input
-      hwmon /sys/devices/virtual/thermal/thermal_zone2/hwmon0/temp1_input
-      hwmon /sys/devices/virtual/thermal/thermal_zone6/hwmon4/temp1_input
-      hwmon /sys/devices/virtual/thermal/thermal_zone4/hwmon2/temp1_input
-    '';
-    levels = ''
-      (0,	0,	55)
-      (1,	48,	60)
-      (2,	50,	61)
-      (3,	52,	63)
-      (4,	56,	65)
-      (5,	59,	66)
-      (7,	63,	32767)
-    '';
-  };
   system.stateVersion = "18.09";
 }
