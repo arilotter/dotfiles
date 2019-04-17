313b340877965be907ea296cc5232a951935c86d
Ari Lotter <arilotter@gmail.com>
2019-04-17 14:24:02 -0400

    testing hard shadows

--- nixpkgs/compton.nix
+++ nixpkgs/compton.nix
@@ -3,7 +3,19 @@ let
 t = import ./theme.nix pkgs;
 in
 {
+  extraOptions = ''
+    shadow = true;
+    no-dnd-shadow = true;
+    no-dock-shadow = true;
+    clear-shadow = true;
+    shadow-radius = 0;
+    shadow-offset-x = 4;
+    shadow-offset-y = 4;
+    shadow-opacity = 1.00;
+    shadow-red = 0.00;
+    shadow-green = 0.00;
+    shadow-blue = 0.00;
+    backend = "glx";
+  '';
   enable = false;
-  activeOpacity = "1.0";
-  inactiveOpacity = "0.9";
 }
