67b97ed8ccd7f3eda68558224deca3980e7ee78b
Ari Lotter <arilotter@gmail.com>
2023-08-26 16:36:03 -0400

    uhhh sharp drivers?

--- nixos/kronos/drivers.nix
+++ nixos/kronos/drivers.nix
@@ -1,31 +1,26 @@
 { pkgs, ... }:
 let
   kernel = pkgs.rpi-kernels.latest.kernel;
-  lcpDriver = pkgs.fetchFromGitHub {
-    owner = "w4ilun";
-    repo = "Sharp-Memory-LCD-Kernel-Driver";
-    rev = "56fc25d3cc0d8d32065b6e54f3901378a1b83dea";
-    sha256 = "sha256-DV59M1x1W+5P7HRCZCFRKrfTlITwTOlV0EQ6ERWnKjs=";
+  sharp-drm-src = pkgs.fetchFromGitHub {
+    owner = "ardangelo";
+    repo = "sharp-drm-driver";
+    rev = "8bdc22653f0555b286c014dbb95bc8064f9693c4";
+    sha256 = "sha256-eRj74G3SNwHgMqF9KYfCGLfaf2g+EZSdpIdnKW+FPwI=";
   };
   sharpDriver = pkgs.stdenv.mkDerivation rec {
-    name = "sharp";
+    name = "sharp-drm";
     version = "0.0.1-${kernel.version}";
-    src = lcpDriver;
+    src = sharp-drm-src;
     hardeningDisable = [ "pic" "format" ];
     nativeBuildInputs = kernel.moduleBuildDependencies;
 
-    KROOT = "${kernel.dev}/lib/modules/${kernel.version}/build";
+    LINUX_DIR = "${kernel.dev}/lib/modules/${kernel.version}/build";
 
-    buildPhase = ''
-      runHook preBuild
-      make KROOT=${KROOT}
-      runHook postBuild
-    '';
 
     installPhase = ''
       runHook preInstall
       mkdir -p $out/lib/modules/${kernel.version}/extra
-      make KROOT=${KROOT} modules_install INSTALL_MOD_PATH=$out
+      INSTALL_MOD_PATH=$out make -C '${LINUX_DIR}' M='$(shell pwd)' modules_install
       runHook postInstall
     '';
   };
@@ -35,7 +30,7 @@ let
   '';
   sharpOverlay = pkgs.runCommand "sharp-overlay" { } ''
     mkdir $out
-    ${pkgs.dtc}/bin/dtc -@ -I dts -O dtb -o $out/sharp.dtbo ${lcpDriver}/sharp.dts
+    ${pkgs.dtc}/bin/dtc -@ -I dts -O dtb -o $out/sharp-drm.dtbo ${sharp-drm-src}/sharp-drm.dts
   '';
   # https://github.com/ardangelo/beepberry-keyboard-driver
   beepy-kbd-src = pkgs.fetchFromGitHub {
@@ -45,15 +40,6 @@ let
     sha256 = "sha256-Hriw9tPOi2ZF/z56ySdXLBHFCeAqs0GMjuyAMswJrK0=";
   };
   keyboardDriver =
-    let
-      type = "BBQ20KBD_PMOD";
-      trackpad = "BBQ20KBD_TRACKPAD_AS_KEYS";
-      int = "BBQX0KBD_USE_INT";
-      intPin = "4";
-      pollPeriod = "40";
-      assignedI2cAddress = "BBQX0KBD_DEFAULT_I2C_ADDRESS";
-      i2cAddress = "0x1F";
-    in
     pkgs.stdenv.mkDerivation rec {
       pname = "beepy-kbd-driver";
       version = "2023-08-25-${kernel.version}";
@@ -63,20 +49,6 @@ let
       nativeBuildInputs = kernel.moduleBuildDependencies ++ [ pkgs.dtc ];
 
       LINUX_DIR = "${kernel.dev}/lib/modules/${kernel.version}/build";
-      KDIR = "${kernel.dev}/lib/modules/${kernel.version}/build";
-      postPatch = ''
-        sed -i  "s/BBQX0KBD_TYPE BBQ.*/BBQX0KBD_TYPE ${type}/g" src/config.h
-        sed -i  "s/BBQ20KBD_TRACKPAD_USE BBQ20KBD_TRACKPAD_AS.*/BBQ20KBD_TRACKPAD_USE ${trackpad}/g" src/config.h
-        sed -i  "s/BBQX0KBD_INT BBQ.*/BBQX0KBD_INT ${int}/g" src/config.h
-        sed -i  "s/BBQX0KBD_INT_PIN .*/BBQX0KBD_INT_PIN ${intPin}/g" src/config.h
-        sed -i  "s/BBQX0KBD_POLL_PERIOD .*/BBQX0KBD_POLL_PERIOD ${pollPeriod}/g" src/config.h
-        sed -i  "s/BBQX0KBD_ASSIGNED_I2C_ADDRESS .*/BBQX0KBD_ASSIGNED_I2C_ADDRESS ${assignedI2cAddress}/g" src/config.h
-      '';
-      buildPhase = ''
-        runHook preBuild
-        make
-        runHook postBuild
-      '';
 
       installPhase = ''
         runHook preInstall
@@ -89,6 +61,8 @@ let
         # Install device tree overlay
         mkdir -p $out/boot/overlays/
         cp ./beepy-kbd.dtbo $out/boot/overlays
+
+        ls -R $out
 
         runHook postInstall
       '';
