d4c1743e26d43de24e213a03de709acd658ca05e
Ari Lotter <arilotter@gmail.com>
2023-08-27 10:38:03 -0400

    wippy

--- nixos/all-systems-configuration.nix
+++ nixos/all-systems-configuration.nix
@@ -78,6 +78,13 @@
     avahi = {
       enable = true;
       nssmdns = true;
+      ipv4 = true;
+      ipv6 = true;
+      publish = {
+        enable = true;
+        addresses = true;
+        workstation = true;
+      };
     };
   };
 
