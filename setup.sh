bc8c219ecf0bed1c277d181e8bad19d49a837eb4
Ari Lotter <arilotter@gmail.com>
2024-07-25 21:55:47 -0400

    remove old crap

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-if [[ ! -d $(realpath ~/.config/nixpkgs) ]]; then ln -s $(realpath ./nixpkgs) $(realpath ~/.config/nixpkgs); fi
-if [[ ! -d $(realpath ~/.config/wal) ]]; then ln -s $(realpath ./wal) $(realpath ~/.config/wal); fi
-if [[ ! -d $(realpath ~/.config/i3) ]]; then mkdir ~/.config/i3; fi
-if [[ ! -d $(realpath ~/.config/i3/blocks) ]]; then ln -s  $(realpath ./i3/blocks) $(realpath ~/.config/i3/blocks); fi
-if [[ ! -f $(realpath ~/bin/) ]]; then ln -s  $(realpath ./bin) $(realpath ~/bin); fi
