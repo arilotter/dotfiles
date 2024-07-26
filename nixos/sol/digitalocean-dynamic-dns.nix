7835eb5eb63bb2bf833ce7c870c6c42df18627ab
Ari Lotter <arilotter@gmail.com>
2024-07-25 23:11:41 -0400

    env fix?

--- nixos/sol/digitalocean-dynamic-dns.nix
+++ nixos/sol/digitalocean-dynamic-dns.nix
@@ -3,18 +3,18 @@ let
   ddns = pkgs.writeShellScriptBin "ddns" (builtins.readFile ./ddns.sh);
 in
 {
-  systemd.user.services.ddns = {
+  systemd.services.ddns = {
     enable = true;
     description = "Dynamic DNS Updater";
     serviceConfig = {
       Type = "simple";
       ExecStart = "${ddns}";
       Restart = "on-failure";
-      Environment = {
-        DIGITALOCEAN_TOKEN_FILE = "/home/sol/digitalocean-token";
-        DOMAIN = "ari.computer";
-        NAME = "home";
-      };
+      Environment = [
+        "DIGITALOCEAN_TOKEN_FILE=/home/sol/digitalocean-token"
+        "DOMAIN=ari.computer"
+        "NAME=home"
+      ];
     };
   };
 }
