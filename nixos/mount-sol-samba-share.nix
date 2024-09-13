923d23dcaaf8543da89023cfbe49870b431487aa
Ari Lotter <arilotter@gmail.com>
2024-09-13 00:00:47 -0400

    switch to tailscale dns mount

--- nixos/mount-sol-samba-share.nix
+++ nixos/mount-sol-samba-share.nix
@@ -1,7 +1,7 @@
 {
   # try to mount the samba share on boot!
   fileSystems."/mnt/storage" = {
-    device = "//sol.local/storage";
+    device = "//sol/storage";
     fsType = "cifs";
     options = [
       "users"
