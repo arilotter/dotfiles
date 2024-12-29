f110b9f0b10c05879b0512fda630f92b000ba106
Ari Lotter <arilotter@gmail.com>
2024-12-28 20:59:11 -0500

    add trickle patch

--- nixos/all-systems-configuration.nix
+++ nixos/all-systems-configuration.nix
@@ -6,7 +6,31 @@
   ...
 }: {
   nixpkgs = {
-    overlays = [inputs.nur.overlays.default];
+    overlays = [
+      inputs.nur.overlays.default
+      (final: prev: {
+        trickle = prev.trickle.overrideAttrs (oldAttrs: {
+
+          preConfigure = ''
+            sed -i 's|libevent.a|libevent.so|' configure
+            sed -i 's/if test "$HAVEMETHOD" = "no"; then/if false; then/' configure
+            sed -i 's/exit(1)/exit(0)/' configure
+            sed -i '1i#define DLOPENLIBC "${final.stdenv.cc.libc}/lib/libc.so.6"' trickle-overload.c
+          '';
+
+          env = (oldAttrs.env or {}) // {
+            NIX_CFLAGS_COMPILE = toString ([
+              "-I${final.libtirpc.dev}/include/tirpc"
+              "-Wno-pointer-sign"
+            ]);
+          };
+
+          patches = (oldAttrs.patches or []) ++ [
+            ./trickle.patch
+          ];
+        });
+      })
+    ];
     config.allowUnfree = true;
   };
 
