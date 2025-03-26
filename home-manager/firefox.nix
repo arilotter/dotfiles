0958488521895244d7caf7cd9a5a07609a748877
Ari Lotter <arilotter@gmail.com>
2025-03-26 16:25:43 -0400

    add Phantom to firefox

--- home-manager/firefox.nix
+++ home-manager/firefox.nix
@@ -10,7 +10,33 @@
           bitwarden
           sponsorblock
           refined-github
+          (buildFirefoxXpiAddon {
+            pname = "phantom-app";
+            version = "25.3.1";
+            addonId = "{7c42eea1-b3e4-4be4-a56f-82a5852b12dc}";
+            url = "https://addons.mozilla.org/firefox/downloads/file/4428642/phantom_app-25.3.1.xpi";
+            sha256 = "sha256-zE2X6dkLiTiILu9w8Xx/9r9lmIuE9R7dk+KfcK3FjhY=";
+            meta = with lib; {
+              mozPermissions = [
+                "activeTab"
+                "alarms"
+                "identity"
+                "storage"
+                "scripting"
+                "tabs"
+                "webRequest"
+                "unlimitedStorage"
+                "http://*/*"
+                "https://*/*"
+              ];
+            };
+          })
         ];
+        userChrome = ''
+          #back-button, #forward-button {
+            display: none;
+          }
+        '';
       };
     };
   };
