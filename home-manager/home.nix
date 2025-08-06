578df35240d50233432a6158491372cbc431f687
Ari Lotter <arilotter@gmail.com>
2025-08-06 15:34:20 -0400

    bye thefuck

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -35,7 +35,6 @@
 
     # command-line utils
     screen
-    thefuck
     killall
     file # file type identification
     graphviz # graph visualization
@@ -128,8 +127,6 @@
 
       set -gx SW_API_HOST "https://local-skyweaver-api.0xhorizon.net"
       set -gx SW_AUTH_TOKEN "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50IjoiMHgyMDM3MTI0NzMwZjRkMmQ1MTI0OGQyYzA1ZDJhZTVjYmQyODhlZDY3IiwiYXBwIjoiU2t5d2VhdmVyIiwiZXhwIjoxNjU1ODM2Nzg4LCJpYXQiOjE2MjQzMDA3ODgsIm9nbiI6Imh0dHBzOi8vbG9jYWwuMHhob3Jpem9uLm5ldCJ9.rFCF1PhcAEJbUdMB4LFd4L6ElqA8rtxMi46gK8fQBB"
-
-      thefuck --alias | source
     '';
   };
   programs.git = {
