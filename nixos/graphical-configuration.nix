49b220c5289c485e250e1692c2e753de7a8b5822
Ari Lotter <arilotter@gmail.com>
2025-08-07 23:14:00 -0400

    idk try wayland stuff

--- nixos/graphical-configuration.nix
+++ nixos/graphical-configuration.nix
@@ -36,6 +36,14 @@
   environment.variables = {
     WLR_NO_HARDWARE_CURSORS = "1";
     NIXOS_OZONE_WL = "1";
+    XDG_SESSION_TYPE = "wayland";
+    XDG_CURRENT_DESKTOP = "Hyprland";
+    XDG_SESSION_DESKTOP = "Hyprland";
+    GDK_BACKEND = "wayland";
+    MOZ_ENABLE_WAYLAND = "1";
+    QT_QPA_PLATFORM = "wayland";
+    QT_QPA_PLATFORM_THEME = "qt6ct";
+    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
   };
 
   services = {
