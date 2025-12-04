4df8436da6ef0be601f79ab340675039b0a20a5c
Ari Lotter <arilotter@gmail.com>
2025-12-04 12:29:13 -0500

    add nixos-cuda cache!

--- nixos/all-systems-configuration.nix
+++ nixos/all-systems-configuration.nix
@@ -49,11 +49,13 @@
         "https://cache.nixos.org/"
         "https://cache.garnix.io"
         "https://nix-community.cachix.org"
+        "https://cache.nixos-cuda.org"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
       ];
 
       # see https://garnix.io/docs/caching#private-caches
